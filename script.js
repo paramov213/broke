ba58a14f563b023207f2ccf97cb0e75c6b2bf8a1
paramov213 <epiksou5@gmail.com>
2026-02-01 17:15:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,64 @@
 const socket = io();
-let me = JSON.parse(localStorage.getItem('tg_session'));
-let currentChat = null;
+let me = JSON.parse(localStorage.getItem('tg_v2'));
+let activeChat = null;
 
-if(me) socket.emit('auth', me);
+if (me) socket.emit('auth', me);
 
 function auth() {
-    const u = document.getElementById('log-u').value;
-    const p = document.getElementById('log-p').value;
-    socket.emit('auth', { username: u, password: p });
-    me = { username: u, password: p };
+    const data = { username: document.getElementById('l-u').value, password: document.getElementById('l-p').value };
+    socket.emit('auth', data);
+    me = data;
 }
 
 socket.on('auth_success', (data) => {
     me = {...me, ...data};
-    localStorage.setItem('tg_session', JSON.stringify(me));
-    document.getElementById('auth-screen').classList.add('hidden');
-    document.getElementById('app').classList.remove('hidden');
-    if(me.username === 'admin') document.getElementById('adm-btn').classList.remove('hidden');
-    renderProfile();
+    localStorage.setItem('tg_v2', JSON.stringify(me));
+    document.getElementById('auth-layer').classList.add('hidden');
+    document.getElementById('app-container').classList.remove('hidden');
+    document.getElementById('dr-nick').innerText = me.nickname;
+    document.getElementById('dr-user').innerText = "@" + me.username;
 });
 
-// Кнопка "Найти и написать" (интегрирована в поиск)
-function searchAndOpen() {
-    const target = document.getElementById('tg-search').value.replace('@','');
-    socket.emit('find_user', target);
+function toggleDrawer() { document.getElementById('side-drawer').classList.toggle('active'); }
+
+function doSearch() {
+    const q = document.getElementById('g-search').value;
+    if (q.length > 1) socket.emit('search_global', q);
 }
 
-socket.on('find_user_res', (user) => {
-    if(!user) return alert("Пользователь не найден");
-    openChat(user.username, user.nickname);
-});
-
-function openChat(username, nickname) {
-    currentChat = username;
-    showView('chat');
-    document.getElementById('active-chat-name').innerText = nickname;
-    document.getElementById('header-avatar').innerText = nickname[0].toUpperCase();
-    document.getElementById('messages').innerHTML = '';
-    
-    // Добавляем в список чатов слева, если еще нет
-    addChatToList(username, nickname);
-}
-
-function addChatToList(username, nickname) {
+socket.on('search_results', (users) => {
     const list = document.getElementById('chat-list');
-    if(document.getElementById('item-' + username)) return;
-    
-    const div = document.createElement('div');
-    div.id = 'item-' + username;
-    div.className = 'chat-item';
-    div.onclick = () => openChat(username, nickname);
-    div.innerHTML = `<div class="chat-avatar">${nickname[0]}</div>
-                     <div><b>${nickname}</b><br><small>@${username}</small></div>`;
-    list.prepend(div);
+    list.innerHTML = '';
+    users.forEach(u => {
+        const item = document.createElement('div');
+        item.className = 'chat-item';
+        item.onclick = () => openChat(u.username, u.nickname);
+        item.innerHTML = `<div class="avatar">${u.avatar}</div><div><b>${u.nickname}</b><br><small>@${u.username}</small></div>`;
+        list.appendChild(item);
+    });
+});
+
+function openChat(user, nick) {
+    activeChat = user;
+    document.getElementById('h-nick').innerText = nick;
+    document.getElementById('h-ava').innerText = nick[0];
+    document.getElementById('messages').innerHTML = '';
 }
 
 function send() {
-    const text = document.getElementById('m-text').value;
-    if(!text || !currentChat) return;
-    socket.emit('private_msg', { from: me.username, to: currentChat, text });
-    document.getElementById('m-text').value = '';
+    const text = document.getElementById('m-input').value;
+    if (!text || !activeChat) return;
+    const msg = { from: me.username, to: activeChat, text, time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) };
+    socket.emit('send_msg', msg);
+    document.getElementById('m-input').value = '';
 }
 
-socket.on('msg_receive', (data) => {
-    if(currentChat === data.from || currentChat === data.to) {
-        const side = data.from === me.username ? 'msg-out' : 'msg-in';
-        document.getElementById('messages').innerHTML += `<div class="msg-bubble ${side}">${data.text}</div>`;
-        const area = document.getElementById('messages');
-        area.scrollTop = area.scrollHeight;
+socket.on('render_msg', (data) => {
+    if (activeChat === data.from || activeChat === data.to) {
+        const type = data.from === me.username ? 'out' : 'in';
+        document.getElementById('messages').innerHTML += `
+            <div class="bubble ${type}">${data.text}<span class="time">${data.time}</span></div>`;
+        const m = document.getElementById('messages');
+        m.scrollTop = m.scrollHeight;
     }
-    // Если нам написали, а мы в другом чате - добавляем в список
-    addChatToList(data.from, data.from);
 });
-
-function showView(id) {
-    document.querySelectorAll('.view').forEach(v => v.classList.add('hidden'));
-    document.getElementById(id + '-view').classList.remove('hidden');
-}
-
-function adm(type) {
-    socket.emit('admin_action', { 
-        adminPass: '565811', 
-        type, 
-        target: document.getElementById('adm-target').value, 
-        val: document.getElementById('adm-val').value 
-    });
-}
