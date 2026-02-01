9b2a0833e0c4f6b07dc78a8c810a70f070cbaf03
paramov213 <epiksou5@gmail.com>
2026-02-01 15:53:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,43 +9,51 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
+// База данных в памяти (после перезагрузки Render обнулится)
 let users = {
-    "admin": { password: "123", nickname: "👑 ADMIN", bio: "Основатель BROKE", nft: [], id: "001", role: "admin", subs: 9999, views: 0, reactions: 0 }
+    "admin": { password: "123", nickname: "👑 ADMIN", nft: [], id: "001", role: "admin", subs: 0, views: 0, reactions: 0 }
 };
+let channels = [];
+let messages = [];
 
 io.on('connection', (socket) => {
     socket.on('auth', (data) => {
-        // Если пользователя нет - создаем, если есть - проверяем пароль
         if (!users[data.username]) {
-            users[data.username] = { 
-                password: data.password, nickname: data.username, 
-                bio: "Новый пользователь", nft: [], id: null, role: "user",
-                subs: 0, views: 0, reactions: 0 
-            };
+            users[data.username] = { password: data.password, nickname: data.username, nft: [], id: null, role: "user", subs: 0, views: 0, reactions: 0 };
         }
-
         if (users[data.username].password === data.password) {
-            socket.emit('auth_success', users[data.username]);
+            socket.join(data.username);
+            socket.emit('auth_success', { ...users[data.username], username: data.username });
         } else {
-            socket.emit('auth_error', 'Неверный пароль!');
+            socket.emit('auth_error', 'Ошибка доступа');
         }
+    });
+
+    socket.on('change_password', (data) => {
+        if (users[data.username]) {
+            users[data.username].password = data.newPass;
+            socket.emit('toast', 'Пароль успешно изменен!');
+        }
+    });
+
+    socket.on('create_channel', (chan) => {
+        channels.push(chan);
+        io.emit('new_channel', channels);
     });
 
     socket.on('admin_action', (data) => {
         if (data.adminPass === '565811') {
-            const target = users[data.targetUser];
-            if (target) {
-                if (data.type === 'gift_nft') target.nft.push(data.nftUrl);
-                if (data.type === 'set_id') target.id = data.val;
-                if (data.type === 'boost_subs') target.subs += parseInt(data.val || 100);
-                if (data.type === 'boost_views') target.views += parseInt(data.val || 500);
-                if (data.type === 'boost_reac') target.reactions += parseInt(data.val || 50);
-                
-                io.emit('update_profile', {username: data.targetUser, data: target});
+            const t = users[data.targetUser];
+            if (t) {
+                if (data.type === 'gift_nft') t.nft.push(data.val);
+                if (data.type === 'set_id') t.id = data.val;
+                if (data.type === 'boost_subs') t.subs += parseInt(data.val);
+                if (data.type === 'boost_views') t.views += parseInt(data.val);
+                if (data.type === 'boost_reac') t.reactions += parseInt(data.val);
+                io.to(data.targetUser).emit('update_me', t);
             }
         }
     });
 });
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, '0.0.0.0', () => console.log(`BROKE Live on ${PORT}`));
+server.listen(10000, '0.0.0.0');
